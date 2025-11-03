697704f286f218f1b986224fffed1094d84a51d6
OriBerger <oriberger12@gmail.com>
2025-11-03 11:41:05 +0200

    initialization

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Tic Tac Toe</h1>
+      <p>Welcome to the Tic Tac Toe game!</p>
+      <div className="Board">
+        {/* Game board will be rendered here */}
+      </div>
+    </div>
+  )
+}
+
+export default App
